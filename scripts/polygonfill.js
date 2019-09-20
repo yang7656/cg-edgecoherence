94bf581de7b923da845ffda482121b43a3329710
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2019-09-20 12:49:24 -0500

    Create polygonfill.js

---
+++ scripts/polygonfill.js
@@ -0,0 +1,88 @@
+var view;
+var ctx;
+var polygons = {
+    convex: {
+        color: '', // choose color here!
+        vertices: [
+            // fill in vertices here!
+        ]
+    },
+    concave: {
+        color: '', // choose color here!
+        vertices: [
+            // fill in vertices here!
+        ]
+    },
+    self_intersect: {
+        color: '', // choose color here!
+        vertices: [
+            // fill in vertices here!
+        ]
+    },
+    interior_hole: {
+        color: '', // choose color here!
+        vertices: [
+            // fill in vertices here!
+        ]
+    }
+};
+
+// Init(): triggered when web page loads
+function Init() {
+    var w = 800;
+    var h = 600;
+    view = document.getElementById('view');
+    view.width = w;
+    view.height = h;
+
+    ctx = view.getContext('2d');
+
+    SelectNewPolygon();
+}
+
+// DrawPolygon(polygon): erases current framebuffer, then draws new polygon
+function DrawPolygon(polygon) {
+    // Clear framebuffer (i.e. erase previous content)
+    ctx.clearRect(0, 0, view.width, view.height);
+
+    // Set line stroke color
+    ctx.strokeStyle = polygon.color;
+
+    // Create empty edge table (ET)
+    var edge_table = [];
+    var i;
+    for (i = 0; i < view.height; i++) {
+        edge_table.push(new EdgeList());
+    }
+
+    // Create empty active list (AL)
+    var active_list = new EdgeList();
+
+
+    // Step 1: populate ET with edges of polygon
+
+
+    // Step 2: set y to first scan line with an entry in ET
+
+
+    // Step 3: Repeat until ET[y] is NULL and AL is NULL
+    //   a) Move all entries at ET[y] into AL
+    //   b) Sort AL to maintain ascending x-value order
+    //   c) Remove entries from AL whose ymax equals y
+    //   d) Draw horizontal line for each span (pairs of entries in the AL)
+    //   e) Increment y by 1
+    //   f) Update x-values for all remaining entries in the AL (increment by 1/m)
+}
+
+// SelectNewPolygon(): triggered when new selection in drop down menu is made
+function SelectNewPolygon() {
+    var polygon_type = document.getElementById('polygon_type');
+    DrawPolygon(polygons[polygon_type.value]);
+}
+
+function DrawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
